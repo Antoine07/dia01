a089cb80690ed73fd24d2e093fc89123a88aeb18
Antoine07 <antoine.lucsko@gmail.com>
2021-10-12 12:38:52 +0200

    combien de i

---
+++ Part2_Structures/Exercices/01_letter_count.py
@@ -0,0 +1,28 @@
+
+phrase="mississippi"
+
+# Très court première correction
+stat1 = { ch: phrase.count(ch) for ch in set(phrase) }
+print(stat1)
+
+# Deuxième correction
+
+# création d'un dictionnaire
+stat2 = {}
+for ch in set(phrase):
+    stat2[ch] = phrase.count(ch)
+
+print(stat2)
+
+# Troisièm correction 
+stat3 = {}
+for ch in set(phrase):
+    count = 0
+    for letter in phrase:
+        if letter == ch:
+            count += 1
+    stat3[ch] = count
+
+print(stat3)
+
+# Est qu'il y a des i dans la phrase et si oui combien.
